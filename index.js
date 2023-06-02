8546a070139184d2780735a5747df740783e147e
Clanie1 <danybarociov@gmail.com>
2023-06-01 17:56:35 -0700

    cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 const port = 3000;
 
 let domainClicks = {};
+
+app.use(cors());
 
 app.get("/:domain", (req, res) => {
   const domain = req.params.domain;
